cfa99adb703a2383e41be98a062355694ef9db94
Erick Carvajal R <neocarvajal@gmail.com>
2023-04-05 00:46:33 -0400

    Update FundProject.sol

--- contracts/FundProject.sol
+++ contracts/FundProject.sol
@@ -29,12 +29,32 @@ contract Project {
         _;
     }
 
-    function fundProject(address payable receiver) public payable difOwner  {
-        receiver.transfer(msg.value);
+    error projectClosed(string message, string _state);
+    event addFunds(string message, address donator, uint amount );
+
+    function fundProject(address payable _donator) public payable difOwner  {
+        require(
+            msg.value > 0,
+            "Debe ingresar un monto mayor a 0" 
+        );
+    
+        if(keccak256(abi.encodePacked(state)) == keccak256(abi.encodePacked("closed"))) {
+            string memory msgError = "El proyecto esta cerrado";
+            revert projectClosed(msgError, state);
+        }else {
+            _donator.transfer(msg.value);
+            uint amount = msg.value;
+            emit addFunds("Gracias por su aporte", _donator, amount);
+        }   
     }
 
+    event changeState(string message, string oldState, string newState);
+
     function changeProjectState(string memory _newState) public onlyOwnerState {
+        string memory oldState = state;
         state = _newState;
+
+        emit changeState("El autor ha cambiado el estado", oldState, state);
     }
 
 }
